60e1583be68f1158b9b4b6c4c1007667ccf768c9
Abhishek N N <7abhisheknn@gmail.com>
2021-10-15 21:56:10 +0530

    login page design

---
+++ lib/widgets/backgrounds/auth_background.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:health_assistant_app/pages/auth_page.dart';
+
+enum AuthMode { signup, login }
+
+class AuthBackground extends StatelessWidget {
+  static const routeName = '/auth';
+
+  const AuthBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
+    // transformConfig.translate(-10.0);
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  const Color.fromRGBO(230, 225, 255, 1.0).withOpacity(0.5),
+                  const Color.fromRGBO(34, 150, 243, 1.0).withOpacity(0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: const [0, 1],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            // ignore: sized_box_for_whitespace
+            child: Container(
+              height: deviceSize.height,
+              width: deviceSize.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 20.0),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blue.shade600,
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                          )
+                        ],
+                      ),
+                      child: const Text(
+                        'Your\nHealth Assistant',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontFamily: 'Anton',
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: deviceSize.width > 600 ? 2 : 1,
+                    child: AuthPage(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
